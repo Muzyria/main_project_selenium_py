58ee0bb208d515f97c2e1be16ad1932263703346
Muzyria <fila090580@gmail.com>
2022-12-17 00:12:56 +0300

    new

--- tests/test_by_product.py
+++ tests/test_by_product.py
@@ -10,6 +10,7 @@ from pages.cart_page import Cart_page
 from pages.client_infomation_page import Client_infomation_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
+from pages.payment_page import Payment_page
 
 
 def test_by_product():
@@ -19,12 +20,18 @@ def test_by_product():
 
     login = Login_page(driver)
     login.authorization()
+
     mp = Main_page(driver)
     mp.select_product()
+
     cp = Cart_page(driver)
     cp.product_confirmation()
+
     cip = Client_infomation_page(driver)
     cip.input_information()
+
+    pp = Payment_page(driver)
+    pp.payment()
 
     time.sleep(5)
 
