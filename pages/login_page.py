1d81e383265e0047893c4ff63c257074d13f9950
“Muzyria” <fila090580@gmail.com>
2022-12-16 17:56:01 +0200

    new

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,12 +4,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from base.base_class import Base
 
 
-class Login_page():
+class Login_page(Base):
+
+    url = 'https://www.saucedemo.com/'
 
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
+
+
+    # Locators
+
+    user_name = '//input[@id="user-name"]'
+    password = '//input[@id="password"]'
+    login_button = '//input[@id="login-button"]'
+
+
+    # Getters
+
+    def get_user_name(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.user_name)))
+
+    def get_password(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.password)))
+
+    def get_login_button(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.login_button)))
+
+
+    # Actions
+
+    def input_user_name(self, user_name):
+        self.get_user_name().send_keys(user_name)
+        print('Input user name')
+
 
     def authorization(self, login_name, login_password):
 
